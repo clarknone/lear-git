d488fa982925f29819e6f07f0883d58b291f17b3
utibe <prophetuty@gmail.com>
2023-05-15 17:20:06 +0100

    Favour.js

--- Favour.js
+++ Favour.js
@@ -1,4 +1,4 @@
-console.log("Omo! javascript is not for the weak")
+console.log("Omo! javascript is not for the weak so bad")
 
 function reverseString(str){
     console.log(str.split("").reverse().join(""));
